271d18c8d1629f46299dfd6196f9dbe343fae5f9
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-02-27 12:40:16 +0100

    feat: update jsnack4

--- jsnack4/script/main.js
+++ jsnack4/script/main.js
@@ -2,17 +2,23 @@
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array
 
+// inizialize empty array
 const array = [];
 
+// iteration for 6 times
 for (let i = 0; i < 6; i++) {
 
+    // user input
     let input = prompt('Inserisci un numero');
+    // convert user input from string to int
     let inputInt = parseInt(input);
     
     if (inputInt % 2 === 1) {
 
+        // add an element to the array
         array.push(inputInt);
     }
 }
 
+// print the array
 console.log(array);
