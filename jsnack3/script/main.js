b32611f5c9bc91fe790889a59b24ee6623cf3118
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-02-27 12:36:44 +0100

    feat: update jsnack3

--- jsnack3/script/main.js
+++ jsnack3/script/main.js
@@ -1,12 +1,19 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
+
+// inizialize and set to 0 variable count
 let count = 0;
 
+// iteration for 10 times
 for (let i = 0; i < 10; i++) {
 
+    // user input
     let input = prompt('Inserisci un numero');
+    // convert user input from string to int
     let inputInt = parseInt(input);
+    // sum user input to count
     count += inputInt;
 }
 
+// print the total sum
 console.log(`La somma dei numeri inseriti è ${count}`);
